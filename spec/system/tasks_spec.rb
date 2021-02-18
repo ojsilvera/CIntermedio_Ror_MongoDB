966d75c6811d1d79e0e70c7d184ef285cfb7a15d
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-18 13:54:31 -0500

    Implementar pruebas de sistema a la vista de new task

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
+require 'ruby_jard'
 
 RSpec.describe "Tasks", type: :system do
   let(:user) { create :user }
   before(:each) { sign_in user }
-
-  before do
-    driven_by(:rack_test)
-  end
 
   describe "GET /tasks" do
     it 'has a correct title' do
@@ -17,13 +14,25 @@ RSpec.describe "Tasks", type: :system do
 
   describe "POST /tasks/new" do
     let!(:category) { create :category }
-    it 'has a correct title' do
+    let!(:participant) { create :user }
+    it 'has a correct title', js: true do
       visit '/tasks/new'
 
       fill_in 'task[name]', with: 'Test 34'
       fill_in 'task[description]', with: 'Test'
       fill_in 'task[due_date]', with: Date.today + 5.day
       select category.name, from: 'task_category_id'
+
+      click_link 'Agregar participante'
+      xpath = '//*[@id="new_task"]/div[1]/div[6]/div[1]'
+
+      within(:xpath, xpath) do
+        select participant.id.to_s, from: 'User'
+        select '1', from: 'Role'
+      end
+      click_botton 'Crear Task'
+
+      expect(page).to have_content('Task was successfully created.')
     end
   end
 end
