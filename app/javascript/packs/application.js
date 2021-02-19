238a6ad04ee34c515a5ef0576d949b39fc7fb326
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-19 15:21:59 -0500

    Login, logout, indes tasks y form new, se le aplico estilos y nuevo diseña

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap/dist/js/bootstrap")
 
+
+import {} from 'jquery-ujs'
 
 // Reference the new application.scss in your app/javascript/packs/application.js
+//= require jquery
+//= require jquery_ujs
+
 import 'bootstrap'
 
 import "roboto-fontface"
