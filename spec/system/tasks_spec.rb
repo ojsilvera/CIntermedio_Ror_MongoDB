5fdf5189719ca1a26c3210b8f98ae3d39e1dba1f
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-22 15:37:36 -0500

    Implementar prueba de systema a añadir participantes en vista new tasks

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -21,15 +21,23 @@ RSpec.describe "Tasks", type: :system do
       fill_in 'task[name]', with: 'Test 34'
       fill_in 'task[description]', with: 'Test'
       fill_in 'task[due_date]', with: Date.today + 5.day
-      select category.name, from: 'task_category_id'
+      # select category.name, from: 'task_category_id'
+      page.execute_script(
+        "document.getElementById('task_category_id').selectize.setValue('#{category.id}')"
+      )
 
       click_link 'Agregar un participante'
-      xpath = '//*[@id="new_task"]/div[1]/div[4]/div[1]'
-
-      within(:xpath, xpath) do
-        select participant.email, from: 'Usuario'
-        select 'responsible', from: 'Rol'
-      end
+      # xpath = '//*[@id="new_task"]/div[1]/div[4]/div[1]'
+      # within(:xpath, xpath) do
+      #   select participant.email, from: 'Usuario'
+      #   select 'responsible', from: 'Rol'
+      # end
+      page.execute_script(
+        "document.querySelector('.selectize.responsible').selectize.setValue('#{participant.id}')"
+      )
+      page.execute_script(
+        "document.querySelector('.selectize.role').selectize.setValue('1')"
+      )
       click_button 'Crear Task'
 
       expect(page).to have_content('Task was successfully created.')
