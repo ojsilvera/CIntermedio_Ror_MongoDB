3151586fcd7ef645ff68fdb24f8f8c12936eb753
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-22 12:21:03 -0500

    Implementar selectize en form new tasks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ import 'bootstrap'
 import '../src/stylesheets/application'
 import '../src/javascripts/main'
 import 'cocoon'
+import 'selectize'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/src/javascripts/main.coffee
+++ app/javascript/src/javascripts/main.coffee
@@ -1,6 +1,12 @@
 window.PLATZI =
   init: ->
     console.log 'test coffe'
+    PLATZI.misc.selectizeByScope('body')
     return
+  misc:
+    selectizeByScope: (selector) ->
+      $(selector).find('.selectize').each (i, el) ->
+        $(el).selectize()
+      return
 $(document).on 'turbolinks:load', ->
   PLATZI.init()
