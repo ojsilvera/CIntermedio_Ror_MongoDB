3878cd7e9e2f07da18b52cc29501190977fa3b54
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-22 10:39:14 -0500

    Completar pruebas de sistemas para tareas

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe "Tasks", type: :system do
   before(:each) { sign_in user }
 
   describe "GET /tasks" do
-    it 'has a correct title' do
+    it 'Aplication list all tasks for a user' do
       visit '/tasks'
-      expect(page).to have_content 'Listing tasks'
+      expect(page).to have_content 'Lista de Tareas'
     end
   end
 
   describe "POST /tasks/new" do
     let!(:category) { create :category }
     let!(:participant) { create :user }
-    it 'has a correct title', js: true do
+    it 'User create a new tasks', js: true do
       visit '/tasks/new'
 
       fill_in 'task[name]', with: 'Test 34'
@@ -23,8 +23,8 @@ RSpec.describe "Tasks", type: :system do
       fill_in 'task[due_date]', with: Date.today + 5.day
       select category.name, from: 'task_category_id'
 
-      click_link 'Agregar participante'
-      xpath = '//*[@id="new_task"]/div[1]/div[6]/div[1]'
+      click_link 'Agregar un participante'
+      xpath = '//*[@id="new_task"]/div[1]/div[4]/div[1]'
 
       within(:xpath, xpath) do
         select participant.email, from: 'Usuario'
